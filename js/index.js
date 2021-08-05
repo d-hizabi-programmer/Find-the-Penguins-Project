6a47e1ec6e5de09ea668818ab7f170f5c0921fc3
Saniya Memon <saniyamemon7862@gmail.com>
2021-08-05 00:01:09 -0400

    ...

--- js/index.js
+++ js/index.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
   //This code will run after your page loads
   var name = prompt("Enter your Name:");
   console.log("Name:", name);
-  $("#firstH2").text("Welcome, " + name);
+  $("#firstH2").text("Welcome, " + name + "...");
   console.log("Value:" + name);
 });
